d4ecc5c555c503439d831e2312025a4e7ddd5b9c
Marcus Ikegami <hikegami.6@gmail.com>
2021-10-13 16:49:18 -0600

    entering a username displays user's repos and issue status

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,73 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a valid GitHub username");
+    }
+
+};
+
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
+            displayRepos(data, user);
             console.log(data);
         });
     });
 };
 
-console.log('outside');
+var displayRepos = function(repos, searchTerm) {
+    
+    // clear old content
+    repoContainerEl.textContent ="";
+    repoSearchTerm.textContent = searchTerm;
 
-getUserRepos();
+    for (var i = 0; i < repos.length; i++) {
+        //format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        //create a container for each repo
+        var repoEl = document.createElement('div');
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        //create a span to hold repo's name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        //append child to container
+        repoEl.appendChild(titleEl);
+
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+        //check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = 
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+
+        }
+
+        repoEl.appendChild(statusEl);
+
+        //append container to dom
+        repoContainerEl.appendChild(repoEl);
+
+    }
+}
